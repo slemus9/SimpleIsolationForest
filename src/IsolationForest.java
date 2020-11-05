929664be115dd78d2cebb1103dd94a3f01a8d194
Sebastián Lemus Cadena <sebastian.slc9@gmail.com>
2020-11-05 11:25:41 -0500

    Adds construction of Isolation Forest

--- src/IsolationForest.java
+++ src/IsolationForest.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 /**
  * Implementation of Isolation Forest based on the document:
@@ -10,32 +10,18 @@ import java.util.function.Function;
 public class IsolationForest {
 
     private double[][] data;
-    private double[][] dataTransposed;
     private String[] features;
     private Map<String, Integer> featuresIdx = new HashMap<>();
 
     public IsolationForest(double[][] data, String[] features) {
         this.data = data;
         this.features = features;
-        initDataTransposed(data);
         initFeatureIndices(features);
     }
 
     private void initFeatureIndices (String[] features) {
         for (int i = 0; i < features.length; i++) {
             featuresIdx.put(features[i], i);
-        }
-    }
-
-    private void initDataTransposed (double[][] data) {
-        int n = data.length;
-        int m = data[0].length;
-        dataTransposed = new double[m][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                dataTransposed[j][i] = data[i][j];
-            }
         }
     }
 
@@ -54,5 +40,42 @@ public class IsolationForest {
             filtered[i] = data[idx];
         }
         return filtered;
+    }
+
+    private int getRandomFeature () {
+        Random random = new Random();
+        return random.nextInt(data[0].length);
+    }
+
+    private double getRandomSplitValue (double[][] data, int feature) {
+        DoubleStream column = Arrays.stream(data[feature]);
+        double max = column.max().orElse(0.0);
+        double min = column.min().orElse(Double.MIN_VALUE);
+        return (max - min)*Math.random() + min;
+    }
+
+    private Pair<double[][], double[][]> splitByRandomFeature (double[][] data, int feature, double splitValue) {
+        double[][] lower = filterBy(data, feature, x -> x <= splitValue);
+        double[][] higher = filterBy(data, feature, x -> x > splitValue);
+        return new Pair<>(lower, higher);
+    }
+
+    private IsolationTree buildIsolationTree (double[][] data, double currentHeight, double maxHeight) {
+        int size = data.length * data[0].length;
+        if (currentHeight >= maxHeight || size <= 1) {
+            return new IsolationLeaf(size);
+        } else {
+            int feature = getRandomFeature();
+            double splitValue = getRandomSplitValue(data, feature);
+            Pair<double[][], double[][]> splits = splitByRandomFeature(data, feature, splitValue);
+            double[][] dataLeft = splits.first;
+            double[][] dataRight = splits.second;
+            return new IsolationTree(
+                    feature,
+                    splitValue,
+                    buildIsolationTree(dataLeft, currentHeight + 1, maxHeight),
+                    buildIsolationTree(dataRight, currentHeight + 1, maxHeight)
+            );
+        }
     }
 }
--- src/Pair.java
+++ src/Pair.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Pair {
+public class Pair <A, B> {
+
+    A first;
+    B second;
+
+    public Pair (A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
 }
